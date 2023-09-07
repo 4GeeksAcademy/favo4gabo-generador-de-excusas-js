b9edd620c256e95733628e02a54b80f3658fa54a
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-09-07 22:48:46 +0000

    excuseJs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // eslint-disable-next-line no-console
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
+
+const excuseGenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  return (
+    who[numerosRamdom(who)] +
+    " " +
+    action[numerosRamdom(action)] +
+    " " +
+    what[numerosRamdom(what)] +
+    " " +
+    when[numerosRamdom(when)]
+  );
+};
+
+function numerosRamdom(array) {
+  return Math.floor(Math.random() * array.length);
+}
